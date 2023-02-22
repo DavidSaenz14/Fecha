faa60c6876f17dc446f9e9e14aec74efc2977925
“DavidSaenz14” <dam115.2023@gmail.com>
2023-02-22 09:26:36 +0100

    proyecto documentado

--- src/examen/Fecha.java
+++ src/examen/Fecha.java
@@ -1,8 +1,22 @@
-package Fecha;
-
+package examen;
+/**
+ * 
+ *@author dam115
+ *@version 1.0
+ */
 public class Fecha {
+	/**
+	 * Es un método público de tipo boolean llamado fechaValida
+	 */
 	public static boolean fechaValida;
-
+	
+	/**
+	 * Este es un método que se encarga de validar si una fecha es válida o no
+	 * @param anio hace referencia al año 
+	 * @param mes hace referencia al mes 
+	 * @param dia hace referencia al dia
+	 * @return tras cumplir todas las condiciones devuelve que la fecha es válida
+	 */
 	public static boolean validarFecha(int anio, int mes, int dia) {
 		fechaValida = false;
 		if ((dia > 0) && (mes > 0) && (anio > 0)) {
